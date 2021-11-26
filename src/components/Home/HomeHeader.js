5e9b4e9b981807346029f849170ba0aa26cce493
petra-misiak <petra.tarokic@gmail.com>
2021-11-26 19:04:53 +0100

    home header fixed styles

--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -10,65 +10,65 @@ import MakeAccount from '../Register/MakeAccount';
 
 const HomeHeader = () => {
     return (
-        <nav className="home-header">
-            <img src={HomeHeroImage} alt="people" className="home-header-img"></img>
-            <div className="home-header-right">
-                <div className="home-header-right-top">
+        <section className="home">
+            <div className="home-img">
+                <img src={HomeHeroImage} alt="people" className="home-img-img"></img>
+            </div>
+            <nav className="home-right">
+                <div className="home-right-top">
                     <BrowserRouter >
-                        <Link to="/" component={Login}>Zaloguj</Link>
-                        <Link to="/" component={MakeAccount}>Założ konto</Link>
+                        <Link to="/" component={Login} className="home-right-top-first">Zaloguj</Link>
+                        <Link to="/" component={MakeAccount} className="home-right-top-first">Założ konto</Link>
                     </BrowserRouter>
-                    {/* </div>
-                <div className="home-header-top2"> */}
-                    <ul className="home-header-right-top-second">
-                        <Scroll
-                            activeClass="active"
-                            to="carousel"
-                            spy={true}
-                            offset={-350}
-                            duration={500}
-                            className="home-header-right-top-second-all">Start</Scroll>
-                        <Scroll
-                            activeClasss="active"
-                            to="carousel"
-                            spy={true}
-                            offset={-350}
-                            duration={500}
-                            className="home-header-right-top-second-all">O co chodzi?</Scroll>
-                        <Scroll
-                            activeClass="active"
-                            to="carousel"
-                            spy={true}
-                            offset={-350}
-                            duration={500}
-                            className="home-header-right-top-second-all">O nas</Scroll>
-                        <Scroll
-                            activeClass="active"
-                            to="carousel"
-                            spy={true}
-                            offset={-350}
-                            duration={500}
-                            className="home-header-right-top-second-all">Fundacja i organizacje</Scroll>
-                        <Scroll
-                            activeClass="active"
-                            to="carousel"
-                            spy={true}
-                            offset={-350}
-                            duration={500}
-                            className="home-header-right-top-second-all">Kontakt</Scroll>
-                    </ul>
                 </div>
-                <div className="home-header-right-bottom">
-                    <h2 className="home-header-right-bottom-title1">Zacznij pomagać!</h2>
-                    <h2 className="home-header-right-bottom-title1">Oddaj niechciane rzeczy w zaufane ręce</h2>
-                    <img src={Decoration} alt='Decoration' className="home-header-right-bottom-decoration"></img>
-                    <div className="home-header-right-buttons">
-                        <button className="home-header-right-buttons-all">ODDAJ RZECZY</button>
-                        <button className="home-header-right-buttons-all">ZORGANIZUJ ZBIÓRKĘ</button>
+                <ul className="home-right-top2">
+                    <Scroll
+                        activeClass="active"
+                        to="carousel"
+                        spy={true}
+                        offset={-350}
+                        duration={500}
+                        className="home-right-top2-all">Start</Scroll>
+                    <Scroll
+                        activeClasss="active"
+                        to="carousel"
+                        spy={true}
+                        offset={-350}
+                        duration={500}
+                        className="home-right-top2-all">O co chodzi?</Scroll>
+                    <Scroll
+                        activeClass="active"
+                        to="carousel"
+                        spy={true}
+                        offset={-350}
+                        duration={500}
+                        className="home-right-top2-all">O nas</Scroll>
+                    <Scroll
+                        activeClass="active"
+                        to="carousel"
+                        spy={true}
+                        offset={-350}
+                        duration={500}
+                        className="home-right-top2-all">Fundacja i organizacje</Scroll>
+                    <Scroll
+                        activeClass="active"
+                        to="carousel"
+                        spy={true}
+                        offset={-350}
+                        duration={500}
+                        className="home-right-top2-all">Kontakt</Scroll>
+                </ul>
+                <div className="home-right-bottom">
+                    <h2 className="home-right-bottom-title1">Zacznij pomagać!</h2>
+                    <h2 className="home-right-bottom-title1">Oddaj niechciane rzeczy w zaufane ręce</h2>
+                    <img src={Decoration} alt='Decoration' className="home-right-bottom-decoration"></img>
+                    <div className="home-right-buttons">
+                        <button className="home-right-buttons-all">ODDAJ <br /> RZECZY</button>
+                        <button className="home-right-buttons-all">ZORGANIZUJ <br />ZBIÓRKĘ</button>
                     </div>
                 </div>
-            </div>
-        </nav >
+            </nav>
+        </section >
     )
 }
 
